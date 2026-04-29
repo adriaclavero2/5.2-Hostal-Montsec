0476b7f47623ff4f181493d8e6496ffa03fcfc38
adriaclavero2 <adriaclavero2@gmail.com>
2026-04-29 19:06:10 +0200

    feat: add security service

--- src/main/java/cat/montsec/hostal/security/SecurityConfig.java
+++ src/main/java/cat/montsec/hostal/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package cat.montsec.hostal.shared.security;
+package cat.montsec.hostal.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
